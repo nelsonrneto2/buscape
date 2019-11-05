3ca145e471eef092c55ad9599958ab0afa4aee05
nelsonrneto2 <nelsonrneto2@users.noreply.github.com>
2019-11-05 15:24:27 -0300

    commit

--- src/app/tela-principal/tela-principal.component.ts
+++ src/app/tela-principal/tela-principal.component.ts
@@ -18,10 +18,7 @@ export class TelaPrincipalComponent implements OnInit {
                 "id": 2321312,
                 "name": "Smartphone Apple iPhone 7 128GB",
                 "images": [
-                    "http://thumbs.buscape.com.br/celular-e-smartphone/smartphone-apple-iphone-7-128gb_600x600-PU98460_1.jpg",
-                    "http://thumbs.buscape.com.br/celular-e-smartphone/smartphone-apple-iphone-7-128gb/__200x400-PU98460_2_c.jpg?v=2347575274",
-                    "http://thumbs.buscape.com.br/celular-e-smartphone/smartphone-apple-iphone-7-128gb/__200x400-PU98460_3_c.jpg?v=318433138",
-                    "http://thumbs.buscape.com.br/celular-e-smartphone/smartphone-apple-iphone-7-128gb/__200x400-PU98460_4_c.jpg?v=33273730"
+                    "../../assets/images/smartphone.jpg"
                 ],
                 "price": {
                     "value": 3509.10,
@@ -34,12 +31,7 @@ export class TelaPrincipalComponent implements OnInit {
             "product": {
                 "id": 9971,
                 "name": "Smart TV Samsung Série 4 UN32J4300AG 32 polegadas LED Plana",
-                "images": [
-                    "http://mthumbs.buscape.com.br/tv/smart-tv-samsung-serie-4-un32j4300ag-32-polegadas-led-plana_600x600-PU95547_1.jpg",
-                    "http://thumbs.buscape.com.br/__400x400-PU95547_2_c.jpg?v=3988579075",
-                    "http://thumbs.buscape.com.br/__400x400-PU95547_4_c.jpg?v=4208003525",
-                    "http://thumbs.buscape.com.br/__231312400x400-PU95547_5_c.jpg?v=1473261122"
-                ],
+                "images": [ "../../assets/images/__400x400-PU95547_2_c.jpg"],
                 "price": {
                     "value": 1139.90,
                     "installments": 10,
@@ -52,7 +44,7 @@ export class TelaPrincipalComponent implements OnInit {
                 "id": 6717132,
                 "name": "Câmera Digital Canon EOS Rebel T5i 18.0 Megapixels",
                 "images": [
-                    "http://mthumbs.buscape.com.br/camera-digital/canon-eos-rebel-t5i-18-0-megapixels_600x600-PU7bf7b_1.jpg"
+                    "../../assets/images/camera.jpg"
                 ],
                 "price": {
                     "value": 1999.20,
@@ -66,7 +58,7 @@ export class TelaPrincipalComponent implements OnInit {
                 "id": 6717131,
                 "name": "Lenovo IdeaPad 310 80UH0004BR Intel Core i7-6500U 2.5 GHz 8192 MB 1024 GB",
                 "images": [
-                    "http://mthumbs.buscape.com.br/notebook/lenovo-ideapad-310-80uh0004br-intel-core-i7-6500u-2-5-ghz-8192-mb-1024-gb_600x600-PU98e6e_1.jpg"
+                    "../../assets/images/notebook.jpg"
                 ],
                 "price": {
                     "value": 2599.00,
